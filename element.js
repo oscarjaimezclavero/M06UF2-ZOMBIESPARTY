e7198d8088dd7e563504c1039ee2dbc6af9e7686
Antonio GM <galvez.madrid.antonio@alumnat.copernic.cat>
2021-01-03 10:54:31 +0100

    AGM. creacion objetos

--- element.js
+++ element.js
@@ -1,33 +1,38 @@
 
 //Objeto Padre
-var Element = function (){
-
+var Element = function (posicion){
+    this.posicion = posicion;  // con esto se tendria acceso externo
+    // var posicion = posicion --> con esto seria privado
+    var descubierto = Boolean(False); // tambien podria ser  "var descubierto = False;"
 }
 
 
 //Objeto Hijo
-var Zombie = function (){
+var Zombie = function (posicion){
     Element.apply(this, arguments);
 }
 
-var Recompensa = function (){
+var Recompensa = function (posicion){
     Element.apply(this, arguments);
 }
 
-var Estrella = function (){
+var Estrella = function (posicion){
     Element.apply(this, arguments);
 }
 
 
 //Objeto Hijo Recompensa
-var DoblePuntuacion = function (){
+var DoblePuntuacion = function (posicion){
     Recompensa.apply(this, arguments);
 }
 
-var MitadZombies = function (){
+var MitadZombies = function (posicion){
     Recompensa.apply(this, arguments);
 }
 
-var VidaExtra = function (){
+var VidaExtra = function (posicion){
     Recompensa.apply(this, arguments);
 }
+
+//CREACION OBJETOS
+//var zombie = new Zombie(posicion); --> Ejemplo
